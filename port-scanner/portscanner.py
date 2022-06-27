b6c4482f1ff5bf1f04b3ed568abcdec0a999bc34
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-06-27 18:34:27 +0530

    Update portscanner.py
    functionality added to scan multiple targets at once.

--- port-scanner/portscanner.py
+++ port-scanner/portscanner.py
@@ -2,6 +2,13 @@ import socket
 from IPy import IP
 from termcolor import colored
 
+def scan(ip):
+	print(f"[*] Scanning the target {ip} ")
+	converted_ip = convert_ip(ip)
+
+	for port in range(75,85):
+		port_scan(converted_ip, port)
+
 def convert_ip(ip):
 	try:
 		IP(ip)
@@ -18,8 +25,10 @@ def port_scan(ipadress, port):
 	except:
 		print(colored(f"[-] The port {port} is closed",'red'))
 
-host = input("[+] Enter the host to scan: ")
-converted_ip = convert_ip(host)
+targets = input("[+] Enter the target/s to scan (seperate multiple targets by ,): ")
 
-for port in range(75,85):
-	port_scan(converted_ip, port)
+if "," in targets:
+	for ip_addr in targets.split(","):
+		scan(ip_addr.strip())
+else:
+	scan(targets)

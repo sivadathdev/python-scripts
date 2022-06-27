e746fbb74a5aaab388cc80b2732ee07f58d606e9
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-06-27 20:11:07 +0530

    Update portscanner.py
    Basic service enumeration functionality added.

--- port-scanner/portscanner.py
+++ port-scanner/portscanner.py
@@ -6,7 +6,7 @@ def scan(ip):
 	print(f"[*] Scanning the target {ip} ")
 	converted_ip = convert_ip(ip)
 
-	for port in range(1,1000):
+	for port in range(1,100):
 		port_scan(converted_ip, port)
 
 def convert_ip(ip):
@@ -16,12 +16,20 @@ def convert_ip(ip):
 	except ValueError:
 		return socket.gethostbyname(ip)
 
+def get_banner(sock):
+	return sock.recv(1024)
+
 def port_scan(ipadress, port):
 	try:
 		s = socket.socket()
 		s.settimeout(0.5)
 		s.connect((ipadress, port))
-		print(colored(f"[+] The port {port} is open.", 'green'))
+
+		try:
+			banner = get_banner(s)
+			print(colored("[+] Open port " + str(port) + ": " + str(banner.decode().strip("\n"))))
+		except:
+			print(colored(f"[+] Open port {port} "))
 	except:
 		pass
 

